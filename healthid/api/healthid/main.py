50d81e8c922142984d3d8584757feabc2fdc7408
spilikin <108782+spilikin@users.noreply.github.com>
2020-07-14 14:36:19 +0200

    Switch API from Flask to FastAPI

--- healthid/api/healthid/main.py
+++ healthid/api/healthid/main.py
@@ -1,18 +1,24 @@
-from flask import Flask, jsonify
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
-# configuration
-DEBUG = True
+app = FastAPI()
 
-# instantiate the app
-app = Flask(__name__)
-app.config.from_object(__name__)
+fake_db = {
+    '99775533@healthid.life': {
+        'uuid':'9e04976d-f74c-4d1d-aa2a-0f9dba7a85ad',
+        'email':'99775533@healthid.life'
+    }
+}
 
-
-# sanity check route
-@app.route('/api/1.0/info', methods=['GET'])
-def info():
-    return jsonify({
+@app.get('/info')
+def public_info():
+    return {
         'description':'HealthID API',
         'version':'1.0'
-    })
+    }
 
+@app.get('/acc/{acc}/')
+def get_account(acc: str):
+    account = fake_db[acc]
+    unless
+    raise HTTPException(status_code=404, detail="Account not found")
