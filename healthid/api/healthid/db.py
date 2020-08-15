d02220fff99acef2c5fee9c0f25d02b190b56b89
spilikin <108782+spilikin@users.noreply.github.com>
2020-08-15 06:18:45 +0200

    Add persistent DB in Docker environment

--- healthid/api/healthid/db.py
+++ healthid/api/healthid/db.py
@@ -1,7 +1,13 @@
 from tinydb import TinyDB, where
-from os import makedirs
+from os import makedirs, environ
 makedirs('.db', exist_ok=True)
-db = TinyDB('.db/db.json')
+
+if "API_ENV" in environ and environ['API_ENV'] == 'DOCKER':
+    db_location = "../healthid_db/db.json"
+else:
+    db_location = "./.db/db.json"
+
+db = TinyDB(db_location)
 
 accounts = db.table('account')
 challenges = db.table('challenge')
